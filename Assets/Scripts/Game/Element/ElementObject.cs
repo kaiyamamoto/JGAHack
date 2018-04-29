fe8c54b42fa0c13d1b6f9f0322c2e4782976a4f2
やまげんPC <dqx_2527@yahoo.co.jp>
2018-04-29 13:59:46 +0900

    要素を持つオブジェクトの準備

---
+++ Assets/Scripts/Game/Element/ElementObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Play.Element
+{
+	// 要素を持つオブジェクトクラス
+	public class ElementObject : MonoBehaviour
+	{
+		[SerializeField, Extensions.ReadOnly]
+		private List<ElementBase> _elementList = null;
+
+		private void Start()
+		{
+			_elementList = new List<ElementBase>();
+			var array = this.GetComponents<ElementBase>();
+			_elementList.AddRange(array);
+		}
+	}
+}
