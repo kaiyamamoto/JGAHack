580420f8aa7d287912689fcfb8befbb17034d889
やまげん <dqx_2527@yahoo.co.jp>
2018-05-01 16:24:41 +0900

    要素の個別選択と個別コピー作成

--- Assets/Scripts/Game/Element/ElementBase.cs
+++ Assets/Scripts/Game/Element/ElementBase.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 namespace Play.Element
 {
-	// 要素の基底クラス
+    // 要素の種類
+    public enum ElementType
+    {
+        None = -1,
+        Move = 0,
+        Action,
+        length
+    }
 
-	[System.Serializable]
-	public abstract class ElementBase : Extensions.MonoBehaviourEx
-	{
-		/// <summary>
-		/// 初期化
-		/// </summary>
-		public abstract void Initialize();
-	}
+    // 要素の基底クラス
+
+    [System.Serializable]
+    public abstract class ElementBase : Extensions.MonoBehaviourEx
+    {
+        // 要素のタイプ
+        protected ElementType _type = ElementType.None;
+
+        public ElementType Type
+        {
+            get { return _type; }
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public abstract void Initialize();
+
+        /// <summary>
+        /// 終了処理
+        /// </summary>
+        public abstract void Discard();
+    }
 }
