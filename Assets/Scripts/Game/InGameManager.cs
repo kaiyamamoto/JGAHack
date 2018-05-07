3174cd6b850f5a231a2dfb91e730694fff59d538
やまげんPC <dqx_2527@yahoo.co.jp>
2018-05-07 22:40:12 +0900

    シーンにカメラ関連を配置

--- Assets/Scripts/Game/InGameManager.cs
+++ Assets/Scripts/Game/InGameManager.cs
@@ -4,23 +4,64 @@ using UnityEngine;
 
 namespace Play
 {
-    // インゲームの管理クラス
-    public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
-    {
-        // UIRoot
-        [SerializeField]
-        private GameObject _uiRoot = null;
-        public GameObject UIRoot
-        {
-            get { return _uiRoot; }
-        }
+	// インゲームの管理クラス
+	public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
+	{
+		// ゲームの状態
+		public enum State
+		{
+			Stand = 0,
+			Play,
+			Pause,
+			End
+		}
 
-        // ElementRoot
-        [SerializeField]
-        private GameObject _elementObjRoot = null;
-        public GameObject ElementObjRoot
-        {
-            get { return _elementObjRoot; }
-        }
-    }
+		// 状態
+		[SerializeField]
+		private State _state = State.Stand;
+		public State GameState
+		{
+			get { return _state; }
+		}
+
+		// UIRoot
+		[SerializeField]
+		private GameObject _uiRoot = null;
+		public GameObject UIRoot
+		{
+			get { return _uiRoot; }
+		}
+
+		// ElementRoot
+		[SerializeField]
+		private GameObject _elementObjRoot = null;
+		public GameObject ElementObjRoot
+		{
+			get { return _elementObjRoot; }
+		}
+
+		/// <summary>
+		/// ゲームの開始
+		/// </summary>
+		public void GameStart()
+		{
+			_state = State.Play;
+		}
+
+		/// <summary>
+		/// ゲームの終わり
+		/// </summary>
+		public void GameEnd()
+		{
+			_state = State.End;
+		}
+
+		/// <summary>
+		/// ゲームの一時停止
+		/// </summary>
+		public void Gamepause()
+		{
+			_state = State.Pause;
+		}
+	}
 }
