e460b734a64e7e2cfdb1e619936565c9523ce8dc
やまげん <dqx_2527@yahoo.co.jp>
2018-05-08 11:17:56 +0900

    向きの要素作成

---
+++ Assets/Scripts/Utils/Enum/GlobalEnum.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public enum Direction
+{
+    Front,
+    Back,
+    Left,
+    Right
+}
+
+public static class DirectionEx
+{
+    public static Vector3 Vector(this Direction dir)
+    {
+        float z = 0.0f;
+        switch (dir)
+        {
+            case Direction.Back:
+                z = 180.0f;
+                break;
+            case Direction.Left:
+                z = 90.0f;
+                break;
+            case Direction.Right:
+                z = 270.0f;
+                break;
+        }
+        return new Vector3(0.0f, 0.0f, z);
+    }
+}
