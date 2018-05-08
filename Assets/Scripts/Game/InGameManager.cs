323c53e5014e2f244f3afb0ca2866e2432588341
やまげん <dqx_2527@yahoo.co.jp>
2018-05-08 09:44:29 +0900

    カメラの設定変更

--- Assets/Scripts/Game/InGameManager.cs
+++ Assets/Scripts/Game/InGameManager.cs
@@ -4,64 +4,68 @@ using UnityEngine;
 
 namespace Play
 {
-	// インゲームの管理クラス
-	public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
-	{
-		// ゲームの状態
-		public enum State
-		{
-			Stand = 0,
-			Play,
-			Pause,
-			End
-		}
+    // インゲームの管理クラス
+    public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
+    {
+        // ゲームの状態
+        public enum State
+        {
+            Stand = 0,
+            Play,
+            Pause,
+            End
+        }
 
-		// 状態
-		[SerializeField]
-		private State _state = State.Stand;
-		public State GameState
-		{
-			get { return _state; }
-		}
+        // 状態
+        [SerializeField, Extensions.ReadOnly]
+        private State _state = State.Stand;
+        public State GameState
+        {
+            get { return _state; }
+        }
 
-		// UIRoot
-		[SerializeField]
-		private GameObject _uiRoot = null;
-		public GameObject UIRoot
-		{
-			get { return _uiRoot; }
-		}
+        // UIRoot
+        [SerializeField]
+        private GameObject _uiRoot = null;
+        public GameObject UIRoot
+        {
+            get { return _uiRoot; }
+        }
 
-		// ElementRoot
-		[SerializeField]
-		private GameObject _elementObjRoot = null;
-		public GameObject ElementObjRoot
-		{
-			get { return _elementObjRoot; }
-		}
+        // ElementRoot
+        [SerializeField]
+        private GameObject _elementObjRoot = null;
+        public GameObject ElementObjRoot
+        {
+            get { return _elementObjRoot; }
+        }
 
-		/// <summary>
-		/// ゲームの開始
-		/// </summary>
-		public void GameStart()
-		{
-			_state = State.Play;
-		}
+        // カメラ
+        [SerializeField]
+        private CameraManager _cameraManager = null;
 
-		/// <summary>
-		/// ゲームの終わり
-		/// </summary>
-		public void GameEnd()
-		{
-			_state = State.End;
-		}
+        /// <summary>
+        /// ゲームの開始
+        /// </summary>
+        public void GameStart()
+        {
+            _state = State.Play;
+        }
 
-		/// <summary>
-		/// ゲームの一時停止
-		/// </summary>
-		public void Gamepause()
-		{
-			_state = State.Pause;
-		}
-	}
+        /// <summary>
+        /// ゲームの終わり
+        /// </summary>
+        public void GameEnd()
+        {
+            _state = State.End;
+        }
+
+        /// <summary>
+        /// ゲームの一時停止
+        /// </summary>
+        public void GamePause()
+        {
+            _state = State.Pause;
+        }
+    }
 }
