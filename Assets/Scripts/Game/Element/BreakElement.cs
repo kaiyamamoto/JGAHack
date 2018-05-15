31bf24a138d01fddfc3bb631ea3dc7a6ddd191bd
Yuhei Matsumura <slovenlyboy@gmail.com>
2018-05-15 10:14:29 +0900

    壊れる特性スクリプト (#33)
    * 壊れる特性スクリプト
    
    * 壊れる修正

---
+++ Assets/Scripts/Game/Element/BreakElement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Extensions;
+using UnityEngine;
+
+
+namespace Play.Element
+{
+    public class BreakElement : MonoBehaviour
+    {
+        //壊れる性質
+
+        //復活できるか？
+        [SerializeField]
+        private bool _canRebirth = false;
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //TODO 接触物判定
+            if (collision.gameObject)
+            {
+                //復活可能なら
+                if (_canRebirth)
+                {
+                    //再生を司るものに情報を送る
+                    //Debug.Log("わが魂をうけとれぇ");
+                    //オブジェクトを非アクティブにする
+                    gameObject.SetActive(false);
+                    return;
+                }
+                //Debug.Log("壊れるぅ");
+                Destroy(gameObject);    
+            }
+        }
+    }
+}
