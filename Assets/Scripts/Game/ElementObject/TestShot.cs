90ecc60803d140e0538cf373af8194fe73f740ee
Yuhei Matsumura <slovenlyboy@gmail.com>
2018-05-16 13:22:17 +0900

    弾置き場関連 (#34)
    * 弾置き場設定
    
    * 弾置き場プレハブ（空オブジェ）の追加

--- Assets/Scripts/Game/ElementObject/TestShot.cs
+++ Assets/Scripts/Game/ElementObject/TestShot.cs
@@ -28,37 +28,50 @@ namespace Play.Enemy
 		//発射カウント
 		[SerializeField]
 		float _shotCount;
+        //弾置き場
+        GameObject _bulletPlace;
 
-		// Use this for initialization
-		void Start()
-		{
-			//発射カウントのリセット
-			_shotCount = _shotInterval;
-		}
+        // Use this for initialization
+        void Start()
+        {
+            //弾置き場探し
+            _bulletPlace = GameObject.Find("BulletPlace");
+            //発射カウントのリセット
+            _shotCount = _shotInterval;         
+        }
 
-		// Update is called once per frame
-		void Update()
-		{
-			//発射カウントダウン
-			_shotCount -= Time.deltaTime;
-			//発射カウント0時
-			if (_shotCount <= 0)
-			{
-				//弾の方向設定（正面）
-				_bulletVel = transform.up;
-				//オフセットの設定(テストなので後日修正)
-				_shotOffset = transform.up * GetComponentInChildren<SpriteRenderer>().bounds.size.x / 1.2f;
-				//// 弾丸の複製
-				GameObject bullets = GameObject.Instantiate(_bullet) as GameObject;
-				// 弾速設定
-				bullets.GetComponent<Rigidbody2D>().velocity = _bulletVel * _bulletSpeed;
-				// 弾丸の位置を調整
-				bullets.transform.position = transform.position + _shotOffset;
-				//弾丸をゲームオブジェクトの子供に設定
-				bullets.transform.parent = transform;
-				//発車時間の再設定
-				_shotCount = _shotInterval;
-			}
-		}
-	}
+        // Update is called once per frame
+        void Update()
+        {
+            //発射カウントダウン
+            _shotCount -= Time.deltaTime;
+            //発射カウント0時
+            if (_shotCount <= 0)
+            {
+                //弾の方向設定（正面）
+                _bulletVel = transform.up;
+                //オフセットの設定(テストなので後日修正)
+                _shotOffset = transform.up　* GetComponent<SpriteRenderer>().bounds.size.x /1.2f;
+                //// 弾丸の複製
+                GameObject bullets = GameObject.Instantiate(_bullet) as GameObject;
+                // 弾速設定
+                bullets.GetComponent<Rigidbody2D>().velocity = _bulletVel * _bulletSpeed;
+                // 弾丸の位置を調整
+                bullets.transform.position = transform.position + _shotOffset;
+                if (_bulletPlace)
+                {
+                    //弾丸を弾置き場の子供に設定
+                    bullets.transform.parent = _bulletPlace.transform;
+                }
+                else
+                {
+                    //弾丸をゲームオブジェクトの子供に設定
+                    bullets.transform.parent = transform;
+                }
+      
+                //発車時間の再設定
+                _shotCount = _shotInterval;
+            }
+        }
+    }
 }
