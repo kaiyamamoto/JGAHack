f13edcf015f8515fa15c3a069f2a760899fce23d
YamaTatsu8 <tatsu8fate@icloud.com>
2018-05-16 16:14:41 +0900

    エフェクト関連

---
+++ Assets/YamaTatsu/Scripts/Noise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Noise : MonoBehaviour {
+
+    IEnumerator GeneratePulseNoise()
+    {
+        for (int i = 0; i <= 180; i += 30)
+        {
+            //歪みの変更
+            GetComponent<Image>().material.SetFloat("_Amount", 0.2f * Mathf.Sin(i * Mathf.Deg2Rad));
+            yield return null;
+        }
+
+    }
+
+    void Update()
+    {
+        StartCoroutine(GeneratePulseNoise());
+    }
+
+}
