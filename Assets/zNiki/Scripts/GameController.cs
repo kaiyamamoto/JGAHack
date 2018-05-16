40279868bf54aee85f1fe5be210dd9ff27f38bf8
Masashi7 <masashi77823@gmail.com>
2018-05-16 13:24:40 +0900

    コントローラー（仮） (#3)
    * Controller　仮作成
    
    * Controller　修正：使用するスクリプトを1つに
    
    * Controller　十字ボタンの処理の削除、暫定的なLR同時押しとコントローラーの入力を検知する処理を追加
    
    * Revert "Controller　十字ボタンの処理の削除、暫定的なLR同時押しとコントローラーの入力を検知する処理を追加"
    
    This reverts commit 9241eb0f9e1464207091d6b67bc15f013f5781fa.
    
    * 十字ボタンの処理の削除、暫定的なLR同時押しとコントローラーの入力を検知する処理を追加
    
    * Controller 一応GameControllerクラス完成
    
    * ゴミファイルの消去と接続されているコントローラーの名前を取得していなかったバグを修正
    
    * デバッグ用のテキストが残ってた…すいません許してください、なんでもしますから…

---
+++ Assets/zNiki/Scripts/GameController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Play
+{
+    public enum Button
+    {
+        A,
+        B,
+        X,
+        Y,
+        L1,
+        R1,
+        BACK,
+        START,
+        L3,
+        R3,
+    }
+
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+    }
+
+    public class GameController : Util.SingletonMonoBehaviour<GameController>
+    {
+        private bool _connected = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            CheckConnect();
+        }
+
+        public bool GetConnectFlag()
+        {
+            CheckConnect();
+
+            return _connected;
+        }
+
+        /// <summary>
+        /// コントローラの名前取得を利用した接続確認
+        /// </summary>
+        private void CheckConnect()
+        {
+            var controllerNames = Input.GetJoystickNames()[0];
+
+            if (controllerNames.Length > 0)
+            {
+                _connected = true;
+            }
+            else
+            {
+                _connected = false;
+            }
+        }
+
+        /// <summary>
+        /// 指定されたボタンが押されているか
+        /// </summary>
+        /// <param name="b">判定したいボタン</param>
+        /// <returns>true:押されている false:押されていない</returns>
+        public bool ButtonDown(Play.Button b)
+        {
+            string button = b.ToString();
+
+            try
+            {
+                if (Input.GetButtonDown(button))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public bool Move(Direction d)
+        {
+
+            switch (d)
+            {
+                case Direction.UP:
+                    if (Input.GetAxis("StickVertical") == 1 || Input.GetAxis("CrossButtonVertical") == 1)
+                    {
+                        return true;
+                    }
+                    break;
+
+                case Direction.DOWN:
+                    if (Input.GetAxis("StickVertical") == -1 || Input.GetAxis("CrossButtonVertical") == -1)
+                    {
+                        return true;
+                    }
+                    break;
+
+                case Direction.LEFT:
+                    if (Input.GetAxis("StickHorizontal") == -1 || Input.GetAxis("CrossButtonHorizontal") == -1)
+                    {
+                        return true;
+                    }
+                    break;
+
+                case Direction.RIGHT:
+                    if (Input.GetAxis("StickHorizontal") == 1 || Input.GetAxis("CrossButtonHorizontal") == 1)
+                    {
+                        return true;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/zNiki/Scripts/test.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Play.GameController con;
+
+    // Use this for initialization
+    void Start () {
+        con = Play.GameController.Instance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (con.GetConnectFlag())
+        {
+            if (con.ButtonDown(Play.Button.A))
+            {
+                Debug.Log("A");
+            }
+            if (con.ButtonDown(Play.Button.B))
+            {
+                Debug.Log("B");
+            }
+            if (con.ButtonDown(Play.Button.X))
+            {
+                Debug.Log("X");
+            }
+            if (con.ButtonDown(Play.Button.Y))
+            {
+                Debug.Log("Y");
+            }
+            if (con.ButtonDown(Play.Button.L1))
+            {
+                Debug.Log("L1");
+            }
+            if (con.ButtonDown(Play.Button.R1))
+            {
+                Debug.Log("R1");
+            }
+            if (con.ButtonDown(Play.Button.BACK))
+            {
+                Debug.Log("BACK");
+            }
+            if (con.ButtonDown(Play.Button.START))
+            {
+                Debug.Log("START");
+            }
+            if (con.ButtonDown(Play.Button.L3))
+            {
+                Debug.Log("L3");
+            }
+            if (con.ButtonDown(Play.Button.R3))
+            {
+                Debug.Log("R3");
+            }
+
+            // バグになる呼び出し
+            if (con.ButtonDown(Play.Button.R3 + 1))
+            {
+                Debug.Log("R3");
+            }
+
+            if (con.Move(Play.Direction.UP))
+            {
+                Debug.Log("UP");
+            }
+            if (con.Move(Play.Direction.DOWN))
+            {
+                Debug.Log("DOWN");
+            }
+            if (con.Move(Play.Direction.LEFT))
+            {
+                Debug.Log("LEFT");
+            }
+            if (con.Move(Play.Direction.RIGHT))
+            {
+                Debug.Log("RIGHT");
+            }
+        }
+        else
+        {
+            Debug.Log("つっかえ！");
+        }
+    }
+}
