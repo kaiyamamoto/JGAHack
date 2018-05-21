62a0158d2141d768bc0b6f2fd7b644577561f7f5
kaiyamamoto <dqx_2527@yahoo.co.jp>
2018-05-21 09:52:04 +0900

    消しゴム機能修正 (#66)

--- Assets/Scripts/Editor/TileMap/Brush/PrefabBrush.cs
+++ Assets/Scripts/Editor/TileMap/Brush/PrefabBrush.cs
@@ -11,51 +11,31 @@ using UnityEditor;
 
 namespace UnityEditor
 {
-    [CustomGridBrush(false, false, false, "Prefab Brush")]
-    public class PrefabBrush : LayerObjectBrush<MonoBehaviour>
-    {
-        public override bool AlwaysCreateOnPaint { get { return true; } }
+	[CustomGridBrush(false, false, false, "Prefab Brush")]
+	public class PrefabBrush : LayerObjectBrush<Object>
+	{
+		public override bool AlwaysCreateOnPaint { get { return true; } }
 
-        /// <summary>
-        /// 配置
-        /// </summary>
-        /// <param name="grid"></param>
-        /// <param name="brushTarget"></param>
-        /// <param name="position"></param>
-        public override void Paint(GridLayout grid, GameObject layer, Vector3Int position)
-        {
-            var target = GetObject(grid, position);
-            if (target != null)
-            {
-                return;
-            }
+		/// <summary>
+		/// 配置
+		/// </summary>
+		/// <param name="grid"></param>
+		/// <param name="brushTarget"></param>
+		/// <param name="position"></param>
+		public override void Paint(GridLayout grid, GameObject layer, Vector3Int position)
+		{
+			var target = GetObject(grid, position);
+			if (target != null)
+			{
+				return;
+			}
 
-            if (ActiveObject != null)
-            {
-                EditorUtil.Select(BrushUtil.GetRootGrid(false).gameObject);
-            }
+			if (ActiveObject != null)
+			{
+				EditorUtil.Select(BrushUtil.GetRootGrid(false).gameObject);
+			}
 
-            base.Paint(grid, layer, position);
-        }
-
-        /// <summary>
-        /// けしけし
-        /// </summary>
-        /// <param name="grid"></param>
-        /// <param name="layer"></param>
-        /// <param name="position"></param>
-        public override void Erase(GridLayout grid, GameObject layer, Vector3Int position)
-        {
-            foreach (var obj in AllObjects)
-            {
-                if (grid.WorldToCell(obj.transform.position) == position)
-                {
-                    DestroyImmediate(obj.gameObject);
-
-                    EditorUtil.Select(BrushUtil.GetRootGrid(false).gameObject);
-                    return;
-                }
-            }
-        }
-    }
+			base.Paint(grid, layer, position);
+		}
+	}
 }
