3d9d2b5b824bd968990103cae7fb8db6efc2c8f3
やまげん <dqx_2527@yahoo.co.jp>
2018-05-21 18:07:15 +0900

    微調整

--- Assets/Scripts/Game/InGameManager.cs
+++ Assets/Scripts/Game/InGameManager.cs
@@ -6,197 +6,185 @@ using UnityEngine.SceneManagement;
 
 namespace Play
 {
-	// インゲームの管理クラス
-	public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
-	{
-		// ゲームの状態
-		public enum State
-		{
-			Stand = 0,
-			Play,
-			Pause,
-			Clear,
-			Over
-		}
-
-		// 状態
-		[SerializeField, Extensions.ReadOnly]
-		private State _state = State.Stand;
-		public State GameState
-		{
-			get { return _state; }
-		}
-
-		// UIRoot
-		[SerializeField]
-		private GameObject _uiRoot = null;
-		public GameObject UIRoot
-		{
-			get { return _uiRoot; }
-		}
-
-		// ElementRoot
-		[SerializeField]
-		private GameObject _elementObjRoot = null;
-		public GameObject ElementObjRoot
-		{
-			get { return _elementObjRoot; }
-		}
-
-		// ステージ管理
-		[SerializeField]
-		private Stage.StageManager _stageManager = null;
-		public StageManager StageManager
-		{
-			get { return _stageManager; }
-		}
-
-		// カメラ管理
-		[SerializeField]
-		private CameraManager _cameraManager = null;
-
-		public CameraManager CameraManager
-		{
-			get { return _cameraManager; }
-		}
-
-		// Pause Panel
-		[SerializeField]
-		private GameObject _pausePlane = null;
-
-		// 復活管理システム
-		[SerializeField]
-		private RebornManager _rebornManager = null;
-
-		void OnGUI()
-		{
-			if (_state == State.Clear)
-			{
-				GUI.Label(new Rect(370, 50, 100, 50), "Clear");
-				// ボタンを表示する
-				if (GUI.Button(new Rect(320, 170, 100, 50), "ReStart"))
-				{
-					GameReLoad();
-				}
-			}
-		}
-
-		void Start()
-		{
-			// Planeを非表示
-			_pausePlane.SetActive(false);
-
-			// ゲームの設定
-			StartCoroutine(StartSetUp());
-
-           
-
-
+    // インゲームの管理クラス
+    public class InGameManager : Util.SingletonMonoBehaviour<InGameManager>
+    {
+        // ゲームの状態
+        public enum State
+        {
+            Stand = 0,
+            Play,
+            Pause,
+            Clear,
+            Over
         }
 
-		private IEnumerator StartSetUp()
-		{
-			// 復活マネージャーの取得
-			_rebornManager = this.GetComponent<RebornManager>();
+        // 状態
+        [SerializeField, Extensions.ReadOnly]
+        private State _state = State.Stand;
+        public State GameState
+        {
+            get { return _state; }
+        }
 
-			// ステージプレハブの設定
-			yield return LoadStage();
+        // UIRoot
+        [SerializeField]
+        private GameObject _uiRoot = null;
+        public GameObject UIRoot
+        {
+            get { return _uiRoot; }
+        }
 
-			// カメラに必要な要素を設定
-			CameraManager.Player = StageManager.Player.gameObject;
-			CameraManager.Goal = StageManager.Goal.gameObject;
+        // ElementRoot
+        [SerializeField]
+        private GameObject _elementObjRoot = null;
+        public GameObject ElementObjRoot
+        {
+            get { return _elementObjRoot; }
+        }
+
+        // ステージ管理
+        [SerializeField]
+        private Stage.StageManager _stageManager = null;
+        public StageManager StageManager
+        {
+            get { return _stageManager; }
+        }
+
+        // カメラ管理
+        [SerializeField]
+        private CameraManager _cameraManager = null;
+
+        public CameraManager CameraManager
+        {
+            get { return _cameraManager; }
+        }
+
+        // Pause Panel
+        [SerializeField]
+        private GameObject _pausePlane = null;
+
+        // 復活管理システム
+        [SerializeField]
+        private RebornManager _rebornManager = null;
+
+        void OnGUI()
+        {
+            if (_state == State.Clear)
+            {
+                GUI.Label(new Rect(370, 50, 100, 50), "Clear");
+                // ボタンを表示する
+                if (GUI.Button(new Rect(320, 170, 100, 50), "ReStart"))
+                {
+                    GameReLoad();
+                }
+            }
+        }
+
+        void Start()
+        {
+            // Planeを非表示
+            _pausePlane.SetActive(false);
+
+            // ゲームの設定
+            StartCoroutine(StartSetUp());
+        }
+
+        private IEnumerator StartSetUp()
+        {
+            // 復活マネージャーの取得
+            _rebornManager = this.GetComponent<RebornManager>();
+
+            // ステージプレハブの設定
+            yield return LoadStage();
+
+            // カメラに必要な要素を設定
+            CameraManager.Player = StageManager.Player.gameObject;
+            CameraManager.Goal = StageManager.Goal.gameObject;
 
             //カメラ初期化
-            StartCoroutine(CameraManager.InitCamera());
+            CameraManager.InitCamera();
 
-
+            _state = State.Play;
         }
 
-		/// <summary>
-		/// ステージの読み込み
-		/// </summary>
-		/// <returns></returns>
-		private IEnumerator LoadStage()
-		{
-			// アセットのロード
-			var stageAsset = Resources.LoadAsync("Stage/TestGrid");
+        /// <summary>
+        /// ステージの読み込み
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator LoadStage()
+        {
+            // アセットのロード
+            var stageAsset = Resources.LoadAsync("Stage/TestGrid");
 
-			// ロード待ち
-			yield return new WaitWhile(() => !stageAsset.isDone);
+            // ロード待ち
+            yield return new WaitWhile(() => !stageAsset.isDone);
 
-			var stageObj = stageAsset.asset as GameObject;
-			var stage = Instantiate(stageObj);
+            var stageObj = stageAsset.asset as GameObject;
+            var stage = Instantiate(stageObj);
 
-			var manager = stage.GetComponent<StageManager>();
+            var manager = stage.GetComponent<StageManager>();
 
-			// ステージマネージャーの設定
-			_stageManager = manager;
-		}
+            // ステージマネージャーの設定
+            _stageManager = manager;
+        }
 
-		/// <summary>
-		/// ゲームの開始
-		/// </summary>
-		public void GameStart()
-		{
-			_state = State.Play;
-		}
+        /// <summary>
+        /// ステージのクリア
+        /// </summary>
+        public void StageClear()
+        {
+            _state = State.Clear;
+        }
 
-		/// <summary>
-		/// ステージのクリア
-		/// </summary>
-		public void StageClear()
-		{
-			_state = State.Clear;
-		}
+        /// <summary>
+        /// ステージの失敗
+        /// </summary>
+        public void StageOver()
+        {
+            StageManager.ReTry();
+        }
 
-		/// <summary>
-		/// ステージの失敗
-		/// </summary>
-		public void StageOver()
-		{
-			StageManager.ReTry();
-		}
+        public void GameReLoad()
+        {
+            Scene loadScene = SceneManager.GetActiveScene();
+            // Sceneの読み直し
+            SceneManager.LoadScene(loadScene.name);
+        }
 
-		public void GameReLoad()
-		{
-			Scene loadScene = SceneManager.GetActiveScene();
-			// Sceneの読み直し
-			SceneManager.LoadScene(loadScene.name);
-		}
+        /// <summary>
+        /// ゲームの一時停止
+        /// </summary>
+        public void GamePause(bool active)
+        {
+            _pausePlane.SetActive(active);
+            if (active)
+            {
+                Time.timeScale = 0f;
+                _state = State.Pause;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                _state = State.Play;
+            }
+        }
 
-		/// <summary>
-		/// ゲームの一時停止
-		/// </summary>
-		public void GamePause(bool active)
-		{
-			_pausePlane.SetActive(active);
-			if (active)
-			{
-				Time.timeScale = 0f;
-				_state = State.Pause;
-			}
-			else
-			{
-				Time.timeScale = 1f;
-				_state = State.Play;
-			}
-		}
+        public Vector3 GetStartPos()
+        {
+            return StageManager.GetStartPos();
+        }
 
-		public Vector3 GetStartPos()
-		{
-			return StageManager.GetStartPos();
-		}
-
-		/// <summary>
-		/// 復活のセット
-		/// </summary>
-		/// <param name="bObj"></param>
-		public void RebornSet(Element.BreakElement bObj)
-		{
-			if (_rebornManager)
-			{
-				_rebornManager.RebornSet(bObj);
-			}
-		}
-	}
+        /// <summary>
+        /// 復活のセット
+        /// </summary>
+        /// <param name="bObj"></param>
+        public void RebornSet(Element.BreakElement bObj)
+        {
+            if (_rebornManager)
+            {
+                _rebornManager.RebornSet(bObj);
+            }
+        }
+    }
 }
--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -1,120 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace Play
 {
-	public class Player : MonoBehaviour
-	{
-		[SerializeField]
-		private float _moveSpeed;
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private float _moveSpeed;
 
-		private Rigidbody2D _rigidbody;
+        private Rigidbody2D _rigidbody;
 
-		public enum State
-		{
-			Alive,
-			Dead
-		}
+        public enum State
+        {
+            Alive,
+            Dead
+        }
 
-		[SerializeField]
-		private State playerState = State.Alive;
+        [SerializeField]
+        private State playerState = State.Alive;
 
-		void Start()
-		{
-			_rigidbody = GetComponent<Rigidbody2D>();
-		}
+        private bool _terrain = true;
 
-		void Update()
-		{
-			if (playerState == State.Dead)
-			{
-				_rigidbody.velocity = Vector2.zero;
-				return;
-			}
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
 
-			var controller = GameController.Instance;
+        void Update()
+        {
+            if (playerState == State.Dead)
+            {
+                _rigidbody.velocity = Vector2.zero;
+                return;
+            }
 
-			// 移動処理とポーズ処理
-			Vector3 tryMove = Vector3.zero;
-			if (controller.GetConnectFlag())
-			{
-				tryMove = ControllerControl(controller);
-				if (controller.ButtonDown(Button.START))
-				{
-					// ポーズ切り替え
-					ChangePause();
-				}
-			}
-			else
-			{
-				tryMove = KeyboardControl();
-				if (Input.GetKeyDown(KeyCode.P))
-				{
-					// ポーズ切り替え
-					ChangePause();
-				}
-			}
+            var controller = GameController.Instance;
 
+            // 移動処理とポーズ処理
+            Vector3 tryMove = Vector3.zero;
+            if (controller.GetConnectFlag())
+            {
+                tryMove = ControllerControl(controller);
+                if (controller.ButtonDown(Button.START))
+                {
+                    // ポーズ切り替え
+                    ChangePause();
+                }
+            }
+            else
+            {
+                tryMove = KeyboardControl();
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    // ポーズ切り替え
+                    ChangePause();
+                }
+            }
 
+            gameObject.GetComponent<PlayerAnimController>().ChangeAnim(tryMove);
+            _rigidbody.velocity = Vector3.ClampMagnitude(tryMove, 1f) * _moveSpeed;
+        }
 
-			gameObject.GetComponent<PlayerAnimController>().ChangeAnim(tryMove);
-			_rigidbody.velocity = Vector3.ClampMagnitude(tryMove, 1f) * _moveSpeed;
-		}
+        /// <summary>
+        /// キーボードでの操作
+        /// </summary>
+        private Vector3 KeyboardControl()
+        {
+            Vector3 tryMove = Vector3.zero;
 
-		/// <summary>
-		/// キーボードでの操作
-		/// </summary>
-		private Vector3 KeyboardControl()
-		{
-			Vector3 tryMove = Vector3.zero;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                tryMove += Vector3Int.left;
+            if (Input.GetKey(KeyCode.RightArrow))
+                tryMove += Vector3Int.right;
+            if (Input.GetKey(KeyCode.UpArrow))
+                tryMove += Vector3Int.up;
+            if (Input.GetKey(KeyCode.DownArrow))
+                tryMove += Vector3Int.down;
 
-			if (Input.GetKey(KeyCode.LeftArrow))
-				tryMove += Vector3Int.left;
-			if (Input.GetKey(KeyCode.RightArrow))
-				tryMove += Vector3Int.right;
-			if (Input.GetKey(KeyCode.UpArrow))
-				tryMove += Vector3Int.up;
-			if (Input.GetKey(KeyCode.DownArrow))
-				tryMove += Vector3Int.down;
+            return tryMove;
+        }
 
-			return tryMove;
-		}
+        /// <summary>
+        /// コントローラーの操作
+        /// </summary>
+        private Vector3 ControllerControl(GameController con)
+        {
+            Vector3 tryMove = Vector3.zero;
 
-		/// <summary>
-		/// コントローラーの操作
-		/// </summary>
-		private Vector3 ControllerControl(GameController con)
-		{
-			Vector3 tryMove = Vector3.zero;
+            if (con.Move(Direction.Left))
+                tryMove += Vector3Int.left;
+            if (con.Move(Direction.Right))
+                tryMove += Vector3Int.right;
+            if (con.Move(Direction.Front))
+                tryMove += Vector3Int.up;
+            if (con.Move(Direction.Back))
+                tryMove += Vector3Int.down;
 
-			if (con.Move(Direction.Left))
-				tryMove += Vector3Int.left;
-			if (con.Move(Direction.Right))
-				tryMove += Vector3Int.right;
-			if (con.Move(Direction.Front))
-				tryMove += Vector3Int.up;
-			if (con.Move(Direction.Back))
-				tryMove += Vector3Int.down;
+            return tryMove;
+        }
 
-			return tryMove;
-		}
-
-		/// <summary>
-		/// ゲームポーズの切り替え
-		/// </summary>
-		private void ChangePause()
-		{
-			var instance = InGameManager.Instance;
-			var stats = instance.GameState;
-			if (stats == InGameManager.State.Pause)
-			{
-				instance.GamePause(false);
-			}
-			else
-			{
-				instance.GamePause(true);
-			}
-		}
-	}
+        /// <summary>
+        /// ゲームポーズの切り替え
+        /// </summary>
+        private void ChangePause()
+        {
+            var instance = InGameManager.Instance;
+            var stats = instance.GameState;
+            if (stats == InGameManager.State.Pause)
+            {
+                instance.GamePause(false);
+            }
+            else
+            {
+                instance.GamePause(true);
+            }
+        }
+    }
 }
