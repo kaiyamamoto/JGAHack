7d5ad5d48503a6d83f667918d482a266f2710661
やまげん <dqx_2527@yahoo.co.jp>
2018-05-24 13:46:30 +0900

    敵は方向を設定されていない場合は正面を向くように設定

--- Assets/Scripts/Game/ElementObject/TestShot.cs
+++ Assets/Scripts/Game/ElementObject/TestShot.cs
@@ -7,111 +7,131 @@ using UnityEngine;
 namespace Play.Enemy
 {
 
-	//簡易射撃テストスクリプト
-	public class TestShot : MonoBehaviour
-	{
-		//生成オブジェクト（弾）
-		[SerializeField]
-		GameObject _bullet;
-		//弾の速さ
-		[SerializeField]
-		float _bulletSpeed;
-		//弾のベクトル
-		[SerializeField]
-		Vector3 _bulletVel;
-		//発射時のズレ
-		[SerializeField]
-		Vector3 _shotOffset;
-		//発射間隔
-		[SerializeField]
-		float _shotInterval = 2;
-		//発射カウント
-		[SerializeField]
-		float _shotCount;
-		//弾置き場
-		GameObject _bulletPlace;
-		// レンダラー
-		SpriteRenderer _renderer = null;
+    //簡易射撃テストスクリプト
+    public class TestShot : MonoBehaviour
+    {
+        //生成オブジェクト（弾）
+        [SerializeField]
+        GameObject _bullet;
+        //弾の速さ
+        [SerializeField]
+        float _bulletSpeed;
+        //弾のベクトル
+        [SerializeField]
+        Vector3 _bulletVel;
+        //発射時のズレ
+        [SerializeField]
+        Vector3 _shotOffset;
+        //発射間隔
+        [SerializeField]
+        float _shotInterval = 2;
+        //発射カウント
+        [SerializeField]
+        float _shotCount;
+        //弾置き場
+        GameObject _bulletPlace;
+        // レンダラー
+        SpriteRenderer _renderer = null;
 
-		// Use this for initialization
-		void Start()
-		{
-			//弾置き場探し
-			_bulletPlace = GameObject.Find("BulletPlace");
-			//発射カウントのリセット
-			_shotCount = _shotInterval;
-			_renderer = GetComponentInParent<SpriteRenderer>();
-		}
+        // Use this for initialization
+        void Start()
+        {
+            //弾置き場探し
+            _bulletPlace = GameObject.Find("BulletPlace");
+            //発射カウントのリセット
+            _shotCount = _shotInterval;
+            _renderer = GetComponentInParent<SpriteRenderer>();
+        }
 
-		// Update is called once per frame
-		void Update()
-		{
-			//発射カウントダウン
-			_shotCount -= Time.deltaTime;
-			//発射カウント0時
-			if (_shotCount <= 0)
-			{
-				var dirs = GetComponents<Play.Element.DiectionTest>();
-				Play.Element.DiectionTest dir = null;
-				foreach (var d in dirs)
-				{
-					if (d.enabled)
-					{
-						dir = d;
-					}
-				}
-				switch (dir.GetDir())
-				{
-					case Direction.Front:
-						//弾の方向設定（正面）
-						_bulletVel = new Vector3(0, 1, 0);
-						//オフセットの設定(テストなので後日修正)
-						_shotOffset = new Vector3(0, _renderer.bounds.size.y, 0);
+        // Update is called once per frame
+        void Update()
+        {
+            //発射カウントダウン
+            _shotCount -= Time.deltaTime;
+            //発射カウント0時
+            if (_shotCount <= 0)
+            {
+                // 向きの取得
+                var dir = GetEnemyDirection();
 
-						break;
-					case Direction.Back:
-						//弾の方向設定（正面）
-						_bulletVel = new Vector3(0, -1, 0);
-						//オフセットの設定(テストなので後日修正)
-						_shotOffset = new Vector3(0, -_renderer.bounds.size.y, 0);
+                switch (dir)
+                {
+                    case Direction.Front:
+                        //弾の方向設定（正面）
+                        _bulletVel = new Vector3(0, 1, 0);
+                        //オフセットの設定(テストなので後日修正)
+                        _shotOffset = new Vector3(0, _renderer.bounds.size.y, 0);
 
-						break;
-					case Direction.Left:
-						//弾の方向設定（正面）
-						_bulletVel = new Vector3(-1, 0, 0);
-						//オフセットの設定(テストなので後日修正)
-						_shotOffset = new Vector3(-_renderer.bounds.size.x, 0, 0);
+                        break;
+                    case Direction.Back:
+                        //弾の方向設定（正面）
+                        _bulletVel = new Vector3(0, -1, 0);
+                        //オフセットの設定(テストなので後日修正)
+                        _shotOffset = new Vector3(0, -_renderer.bounds.size.y, 0);
 
-						break;
-					case Direction.Right:
-						//弾の方向設定（正面）
-						_bulletVel = new Vector3(1, 0, 0);
-						//オフセットの設定(テストなので後日修正)
-						_shotOffset = new Vector3(_renderer.bounds.size.x, 0, 0);
+                        break;
+                    case Direction.Left:
+                        //弾の方向設定（正面）
+                        _bulletVel = new Vector3(-1, 0, 0);
+                        //オフセットの設定(テストなので後日修正)
+                        _shotOffset = new Vector3(-_renderer.bounds.size.x, 0, 0);
 
-						break;
-				}
+                        break;
+                    case Direction.Right:
+                        //弾の方向設定（正面）
+                        _bulletVel = new Vector3(1, 0, 0);
+                        //オフセットの設定(テストなので後日修正)
+                        _shotOffset = new Vector3(_renderer.bounds.size.x, 0, 0);
 
-				// 弾丸の複製
-				GameObject bullets = GameObject.Instantiate(_bullet) as GameObject;
-				// 弾速設定
-				bullets.GetComponent<Rigidbody2D>().velocity = _bulletVel * _bulletSpeed;
-				// 弾丸の位置を調整
-				bullets.transform.position = transform.position + _shotOffset;
-				if (_bulletPlace)
-				{
-					//弾丸を弾置き場の子供に設定
-					bullets.transform.parent = _bulletPlace.transform;
-				}
-				else
-				{
-					//弾丸をゲームオブジェクトの子供に設定
-					bullets.transform.parent = transform;
-				}
+                        break;
+                }
 
-				//発車時間の再設定
-				_shotCount = _shotInterval;
-			}
-		}
-	}
+                // 弾丸の複製
+                GameObject bullets = GameObject.Instantiate(_bullet) as GameObject;
+                // 弾速設定
+                bullets.GetComponent<Rigidbody2D>().velocity = _bulletVel * _bulletSpeed;
+                // 弾丸の位置を調整
+                bullets.transform.position = transform.position + _shotOffset;
+                if (_bulletPlace)
+                {
+                    //弾丸を弾置き場の子供に設定
+                    bullets.transform.parent = _bulletPlace.transform;
+                }
+                else
+                {
+                    //弾丸をゲームオブジェクトの子供に設定
+                    bullets.transform.parent = transform;
+                }
+
+                //発車時間の再設定
+                _shotCount = _shotInterval;
+            }
+        }
+
+        /// <summary>
+        /// 現在の向きを取得
+        /// </summary>
+        /// <returns></returns>
+        private Direction GetEnemyDirection()
+        {
+            var dirs = GetComponents<Play.Element.DiectionTest>();
+            Play.Element.DiectionTest dir = null;
+            foreach (var d in dirs)
+            {
+                if (d.enabled)
+                {
+                    dir = d;
+                }
+            }
+
+            if (dir == null)
+            {
+                // 設定されていない場合正面
+                return Direction.Front;
+            }
+
+            // 向きを返す
+            return dir.GetDir();
+        }
+    }
 }
