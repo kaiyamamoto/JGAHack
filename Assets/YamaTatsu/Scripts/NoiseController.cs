725cd6c2ec4d0a978c8250c8f58f8bbd7dea784a
YamaTatsu8 <tatsu8fate@icloud.com>
2018-05-25 09:13:42 +0900

    ノイズいじり

--- Assets/YamaTatsu/Scripts/NoiseController.cs
+++ Assets/YamaTatsu/Scripts/NoiseController.cs
@@ -26,10 +26,10 @@ public class NoiseController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetMouseButtonDown(0))
-        {
-            StartCoroutine(GeneratePulseNoise());
-        }
+        GeneratePulseNoise();
 		
 	}
+
+    
+
 }
