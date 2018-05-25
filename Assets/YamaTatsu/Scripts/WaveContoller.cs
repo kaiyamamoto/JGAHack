448cde28bf2de117f7b23d8afa8ce480154f01d1
YamaTatsu8 <tatsu8fate@icloud.com>
2018-05-25 09:32:19 +0900

    コンフリクトの回避

--- Assets/YamaTatsu/Scripts/WaveContoller.cs
+++ Assets/YamaTatsu/Scripts/WaveContoller.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class WaveContoller : MonoBehaviour {
 
     //ターゲットの座標
+<<<<<<< 725cd6c2ec4d0a978c8250c8f58f8bbd7dea784a
     [SerializeField]
+=======
+>>>>>>> 軌跡の作成
     private Vector3 targetPos = new Vector3(0, 0, 0);
 
     //スピード
@@ -29,7 +32,11 @@ public class WaveContoller : MonoBehaviour {
         //計算
         Vector3 pos = targetPos - transform.position;
         //正規化
+<<<<<<< 725cd6c2ec4d0a978c8250c8f58f8bbd7dea784a
         transform.position += pos.normalized * _speed;
+=======
+        transform.position += pos.normalized * 0.4f;
+>>>>>>> 軌跡の作成
 
     }
 
