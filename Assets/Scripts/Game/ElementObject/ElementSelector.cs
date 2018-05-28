1d9faad2eb5e4eece91731dd0e09d40cf26d474d
やまげん <dqx_2527@yahoo.co.jp>
2018-05-28 15:15:57 +0900

    ターゲットの選択処理を変更

--- Assets/Scripts/Game/ElementObject/ElementSelector.cs
+++ Assets/Scripts/Game/ElementObject/ElementSelector.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Play.Element;
 using Extensions;
+using Play.LockOn;
 
 using System.Reflection;
 
@@ -29,19 +30,21 @@ namespace Play
 
         // TODO:選択したオブジェクトの要素テキスト
         [SerializeField]
-        private Text _elementText;
+        public Text _elementText;
         [SerializeField, ReadOnly]
         private Text[] _textList = null;
 
-        private List<ElementObject> _targetList = null;
-        private int _targetNum = 0;
-
         // ターゲットのゲームオブジェクト
         [SerializeField]
-        private GameObject _target = null;
+        public GameObject _target = null;
 
         // ロックオン
         private LockOn.LockOn _lockOn = null;
+
+        public LockOn.LockOn LockOnObj
+        {
+            get { return _lockOn; }
+        }
 
         void Start()
         {
@@ -53,20 +56,12 @@ namespace Play
             _textList = new Text[num];
 
             // ロックオン関連の初期化
-            _lockOn = new LockOn.LockOn();
-            if (_targetList == null)
-            {
-                _targetList = _lockOn.GetLockOnList();
-                _targetNum = _lockOn.GetNearObjOnList();
-            }
+            _lockOn = this.gameObject.AddComponent<LockOn.LockOn>();
         }
 
         void Update()
         {
             // 選択
-            // 選択したオブジェクトを取得
-            var selectObj = GetClickObject();
-            TargetObject(selectObj);
             var con = GameController.Instance;
 
             var isTarget = TargetChoice.None;
@@ -91,17 +86,17 @@ namespace Play
             if (isTarget != TargetChoice.None)
             {
                 // カメラにエレメントオブジェクトが移っているとき探す
-                if (_lockOn.CheckOnScreenAll())
+                if (LockOnObj.CheckOnScreenAll())
                 {
                     // 次のターゲットオブジェクトを取得
                     if (isTarget != TargetChoice.Next)
                     {
                         // 選択
-                        TargetObject(GetTarget(1));
+                        TargetObject(LockOnObj.GetTarget(1));
                     }
                     else if (isTarget != TargetChoice.Front)
                     {
-                        TargetObject(GetTarget(-1));
+                        TargetObject(LockOnObj.GetTarget(-1));
                     }
                 }
             }
@@ -126,69 +121,17 @@ namespace Play
         }
 
         /// <summary>
-        /// ターゲットの取得
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>        
-        private ElementObject GetTarget(int num)
-        {
-            _targetNum += num;
-
-            if (_targetList.Count <= _targetNum)
-            {
-                _targetNum = 0;
-            }
-            else if (_targetNum < 0)
-            {
-                _targetNum = _targetList.Count - 1;
-            }
-
-            var obj = _targetList[_targetNum];
-            //オブジェクトが「missing」（破壊済み）の場合
-            if (obj == null)
-            {
-                //消すオブジェ
-                //該当オブジェクトを排斥対象に
-                var exclusionObj = obj;
-                //リストから排斥
-                _targetList.Remove(exclusionObj);
-                //再起呼び出し
-                obj = GetTarget(num);
-            }
-            //オブジェクトのチェックが外れている（再生待機）時
-            else if (obj.gameObject.activeInHierarchy == false)
-            {
-                //再起呼び出し
-                obj = GetTarget(num);
-            }
-
-            //カメラ内に入っていなければ飛ばし
-            if (_lockOn.CheckOnScreen(obj.transform.position) == false)
-            {
-                //再起呼び出し
-                obj = GetTarget(num);
-            }
-
-            // 思い出し中はタゲしない
-            if (obj.Stats == ElementObject.ElementStates.Remember)
-            {
-                //再起呼び出し
-                obj = GetTarget(num);
-            }
-
-            return obj;
-        }
-
-        /// <summary>
         /// オブジェクトをターゲット
         /// </summary>
         private void TargetObject(ElementObject obj)
         {
-            if (obj)
+            if (obj == null)
             {
-                // 要素をターゲット
-                TargetElementObject(obj);
+                return;
             }
+
+            // 要素をターゲット
+            TargetElementObject(obj);
         }
 
         /// <summary>
@@ -198,63 +141,37 @@ namespace Play
         private void TargetElementObject(ElementObject elementObj)
         {
             // ターゲット解除
-            TargetRelease();
+            TargetUIRelease();
 
             // ターゲット
             _targetObject = elementObj;
 
-            // TODO: 仮で選択したオブジェクトにテキストを付与
-            // ======================================================
-            // 子に要素追加
-            var text = GameObject.Instantiate(_elementText);
-            _targetObject.transform.SetChild(text.gameObject);
-            // ターゲットマーカー作成
-            var obj = Instantiate(_target);
-            text.transform.SetChild(obj);
-            text.transform.localPosition = Vector3.zero;
-            text.gameObject.AddComponent<Canvas>();
-            var scaler = text.gameObject.AddComponent<CanvasScaler>();
-            scaler.dynamicPixelsPerUnit = 20;
-            text.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
-            text.transform.localScale = new Vector3(0.3f, 0.3f, 1.0f);
-            text.fontSize = 1;
-            text.alignment = TextAnchor.MiddleLeft;
+            var target = _targetObject.gameObject.AddComponent<TargetObject>();
+            target.SetSelector(this);
 
-            // テキスト変更
-
-            text.text = string.Empty;
-            foreach (var element in _targetObject.ElementList)
-            {
-                if (element)
-                {
-                    text.text += element.Type.ToString() + "\n";
-                }
-            }
-            if (text.text == string.Empty)
-            {
-                text.text = "NoneElement";
-            }
-            // ======================================================
         }
 
         /// <summary>
         /// オブジェクトへのターゲットを解除
         /// </summary>
-        private void TargetRelease()
+        public void TargetRelease()
         {
-            // TODO: 追加したテキスト削除
-            if (_targetObject)
-            {
-                var childs = _targetObject.transform.GetAllChild();
-                foreach (var c in childs)
-                {
-                    if (c.name == "ElementText(Clone)")
-                    {
-                        Destroy(c.gameObject);
-                    }
-                }
-            }
             _targetObject = null;
+        }
+        public void TargetUIRelease()
+        {
+            if (_targetObject == null)
+            {
+                return;
+            }
+
+            // タゲUIなど解除
+            var target = _targetObject.GetComponent<TargetObject>();
+
+            if (target)
+            {
+                target.Release();
+            }
         }
 
         /// <summary>
@@ -265,7 +182,7 @@ namespace Play
             SelectRelease();
             _container.ReceiveAllElement(_targetObject.ElementList);
             // ターゲット解除
-            TargetRelease();
+            TargetUIRelease();
             // TODO: テキスト追加
             AddText(_container.List.ToArray());
         }
@@ -325,7 +242,7 @@ namespace Play
             selectObj.ReceiveAllElement(_container.List.ToArray());
 
             // ターゲット解除
-            TargetRelease();
+            TargetUIRelease();
         }
         // TODO: 要素テキスト追加
         private void AddText(ElementBase[] elements)
---
+++ Assets/Scripts/Game/LockOn/TargetObject.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Extensions;
+using Play.Element;
+
+namespace Play.LockOn
+{
+    public class TargetObject : MonoBehaviourEx
+    {
+        // 参照しているセレクター
+        private ElementSelector _selector = null;
+
+        // 要素オブジェクト
+        private Element.ElementObject _elementObj = null;
+
+        public ElementObject ElementObj
+        {
+            get
+            { return _elementObj; }
+        }
+
+        public ElementSelector Selector
+        {
+            get { return _selector; }
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        void Setting()
+        {
+            _elementObj = GetComponent<Element.ElementObject>();
+
+            // TODO: 仮で選択したオブジェクトにテキストを付与
+            // ======================================================
+            // 子に要素追加
+            var text = GameObject.Instantiate(Selector._elementText);
+            transform.SetChild(text.gameObject);
+            // ターゲットマーカー作成
+            var obj = Instantiate(Selector._target);
+            text.transform.SetChild(obj);
+            text.transform.localPosition = Vector3.zero;
+            text.gameObject.AddComponent<Canvas>();
+            var scaler = text.gameObject.AddComponent<CanvasScaler>();
+            scaler.dynamicPixelsPerUnit = 20;
+            text.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+            text.transform.localScale = new Vector3(0.3f, 0.3f, 1.0f);
+            text.fontSize = 1;
+            text.alignment = TextAnchor.MiddleLeft;
+
+
+            // ターゲットされていることを自覚させる
+
+            // テキスト変更
+
+            text.text = string.Empty;
+            foreach (var element in ElementObj.ElementList)
+            {
+                if (element)
+                {
+                    text.text += element.Type.ToString() + "\n";
+                }
+            }
+            if (text.text == string.Empty)
+            {
+                text.text = "NoneElement";
+            }
+            // ======================================================
+        }
+
+        /// <summary>
+        /// セレクターの設定
+        /// </summary>
+        /// <param name="selector"></param>
+        public void SetSelector(ElementSelector selector)
+        {
+            _selector = selector;
+
+            Setting();
+        }
+
+        void Update()
+        {
+            var lockOn = Selector.LockOnObj;
+            if (!lockOn.CheckOnScreen(transform.position))
+            {
+                // 画面外に出たらターゲット解除
+                Release();
+            }
+        }
+
+        public void Release()
+        {
+            // 選択解除
+            var childs = transform.GetAllChild();
+            foreach (var c in childs)
+            {
+                if (c.name == "ElementText(Clone)")
+                {
+                    Destroy(c.gameObject);
+                }
+            }
+            Destroy(this);
+
+            _selector.TargetRelease();
+        }
+    }
+}
