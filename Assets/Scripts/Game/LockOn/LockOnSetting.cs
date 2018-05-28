adb99aee8308373e5cdea83f922eac2a80564f5d
やまげん <dqx_2527@yahoo.co.jp>
2018-05-28 15:19:32 +0900

    ロックオン関連のプロパティ設定の準備

---
+++ Assets/Scripts/Game/LockOn/LockOnSetting.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOnSetting : ScriptableObject
+{
+    [SerializeField]
+    private Vector3 _test;
+}
