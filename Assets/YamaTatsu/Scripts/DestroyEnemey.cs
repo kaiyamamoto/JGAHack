07e0148faa82204a17564d24f57ca261cfb1afaa
YamaTatsu8 <tatsu8fate@icloud.com>
2018-05-30 19:28:31 +0900

    エフェクト修正

---
+++ Assets/YamaTatsu/Scripts/DestroyEnemey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemey : MonoBehaviour {
+
+    //
+    [SerializeField]
+    private float _time = 1.0f;
+    //カウント
+    private float _timeCount = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+        //
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _timeCount += Time.deltaTime;
+
+        //
+        if(_timeCount > _time)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+	}
+}
