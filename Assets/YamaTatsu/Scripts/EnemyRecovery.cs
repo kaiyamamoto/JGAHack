b6f7b5a99cf2578d4e75463df9b5326083d7c2db
YamaTatsu8 <tatsu8fate@icloud.com>
2018-06-01 09:40:07 +0900

    EnemyRecoveryの修正、秒数の変更を可能に

---
+++ Assets/YamaTatsu/Scripts/EnemyRecovery.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyRecovery : MonoBehaviour {
+
+    private Image gage;
+   
+    //カウント設定
+    [SerializeField]
+    private float _timeMax = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        gage = transform.FindChild("gage").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gage.fillAmount += Time.deltaTime / _timeMax;
+        //Debug.Log(gage.fillAmount);
+		
+	}
+}
