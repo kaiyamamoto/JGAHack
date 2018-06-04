b7c437c226551410dc6dd2b307a15e23500fa1ff
Yuhei Matsumura <slovenlyboy@gmail.com>
2018-06-04 10:02:46 +0900

    操作ガイドUIの実装 (#117)
    * 操作ガイドbase作成
    
    * 操作ガイドUIパネル作成
    
    * 操作UIの調整
    
    * ガイドパネルプレハブの修正、スクリプト内のテスト用記述を削除

---
+++ Assets/Scripts/Game/UI/GuidUI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+
+namespace Play
+{
+
+    public class GuidUI : MonoBehaviour
+    {
+
+        //案内状況
+        public enum GUID_STEP
+        {
+            Normal,
+            Lockon
+        }
+        //表示するUI（こども）
+        [SerializeField, ReadOnly]
+        private GameObject[] _uiSet;
+
+
+
+        void Start()
+        {
+            //全行取得
+            _uiSet = transform.GetAllChild();
+            //表示
+            ShowAll();
+        }
+
+
+        //非表示
+        public void HideAll()
+        {
+            for (int i = 0; i < _uiSet.Length; i++)
+            {
+                _uiSet[i].SetActive(false);
+
+            }
+        }
+
+        //表示
+        public void ShowAll()
+        {
+            for (int i = 0; i < _uiSet.Length; i++)
+            {
+                _uiSet[i].SetActive(true);
+            }
+        }
+
+
+        //案内状況に応じて表示変化
+        public void ChangeGuid(GUID_STEP step)
+        {
+            switch (step)
+            {
+                case GUID_STEP.Normal:
+                    _uiSet[0].GetComponent<KeyUI>().GuidUISet(KeyUI.GUID_ID.Move);
+                    _uiSet[1].GetComponent<KeyUI>().GuidUISet(KeyUI.GUID_ID.LockON);
+                    _uiSet[2].SetActive(false);
+                    break;
+
+                case GUID_STEP.Lockon:
+                    _uiSet[0].GetComponent<KeyUI>().GuidUISet(KeyUI.GUID_ID.ChangeLock);
+                    _uiSet[1].GetComponent<KeyUI>().GuidUISet(KeyUI.GUID_ID.Copy);
+                    _uiSet[2].GetComponent<KeyUI>().GuidUISet(KeyUI.GUID_ID.Paste);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/UI/KeyUI.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Extensions;
+
+
+public class KeyUI : MonoBehaviour {
+
+    //ガイドID
+    public enum GUID_ID
+    {
+        LockON,
+        Move,
+        Copy,
+        Paste,
+        ChangeLock
+    }
+
+    //アイコンID
+    public enum ICON_ID
+    {
+        
+        Copy,
+        LockOnL,
+        LockOnR,
+        Paste,
+     
+    }
+
+    //表示文字
+    [SerializeField, ReadOnly]
+    Text _text;
+    //表示アイコン
+    [SerializeField, ReadOnly]
+    Image _icon;
+    //表示アイコン2
+    [SerializeField, ReadOnly]
+    Image _icon2;
+    //アイコン集
+    [SerializeField,ReadOnly]
+    Sprite[] _images;
+
+
+    // Use this for initialization
+    void Start () {
+        //テキスト取得
+        _text = transform.Find("Text").GetComponent<Text>();
+        //アイコン取得
+        _icon = transform.Find("Icon").GetComponent<Image>();
+        //アイコン取得
+        _icon2 = transform.Find("Icon2").GetComponent<Image>();
+        //リソースからアイコンゲット
+        _images =  Resources.LoadAll<Sprite>("Icons");
+    }
+	
+	
+    //メッセージの変更
+    void SetGuidText(string text)
+    {
+        _text.text = text;
+    }
+
+    //アイコンの変更
+    public virtual void SetIcon(Sprite sprite)
+    {
+        _icon.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(205, 0, 0);
+        _icon.sprite = sprite;
+        _icon2.gameObject.SetActive(false);
+    }
+
+    //アイコンの変更(二枚左右出し)
+    public virtual void SetIcon(Sprite sprite,Sprite sprite2)
+    {
+        _icon.sprite = sprite;
+        _icon.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(145, 0, 0);
+
+        _icon2.gameObject.SetActive(true);
+        _icon2.sprite = sprite2;
+        _icon2.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(245, 0, 0);
+    }
+
+
+    //ガイド内容のセット
+    public void GuidUISet(GUID_ID id)
+    {
+        this.gameObject.SetActive(true);
+        switch (id)
+        {
+            case GUID_ID.Move:
+                //テキスト変更
+                SetGuidText("移動");
+                //アイコン変更
+                SetIcon(_images[(int)ICON_ID.Copy]);
+                break;
+
+            case GUID_ID.LockON:
+                //テキスト変更
+                SetGuidText("ロックオン");
+                //アイコン変更
+                SetIcon(_images[(int)ICON_ID.LockOnR]);
+                break;
+
+            case GUID_ID.ChangeLock:
+                //テキスト変更
+                SetGuidText("ロックオン切り替え");
+                //アイコン変更
+                SetIcon(_images[(int)ICON_ID.LockOnL],_images[(int)ICON_ID.LockOnR]);
+                break;
+
+            case GUID_ID.Copy:
+                //テキスト変更
+                SetGuidText("コピー");
+                //アイコン変更
+                SetIcon(_images[(int)ICON_ID.Copy]);
+                break;
+
+            case GUID_ID.Paste:
+                //テキスト変更
+                SetGuidText("ペースト");
+                //アイコン変更
+                SetIcon(_images[(int)ICON_ID.Paste]);
+                break;
+        }     
+    }  
+}
