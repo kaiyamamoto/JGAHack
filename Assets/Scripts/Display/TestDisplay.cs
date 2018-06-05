647aed661ac16298f0af96984b066b23c75836f4
やまげんPC <dqx_2527@yahoo.co.jp>
2018-06-05 22:45:56 +0900

    ゲーム終了ポップアップ仮作成

--- Assets/Scripts/Display/TestDisplay.cs
+++ Assets/Scripts/Display/TestDisplay.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 namespace Util.Display
 {
-    public sealed class TestDisplay : DisplayBase
-    {
-        // 遷移するディスプレイ
-        [SerializeField]
-        private DisplayBase _changeDisplay = null;
+	public sealed class TestDisplay : DisplayBase
+	{
+		// 遷移するディスプレイ
+		[SerializeField]
+		private DisplayBase _changeDisplay = null;
 
-        // 遷移する際押すマウスの対応した番号
-        [SerializeField]
-        private int _button = 1;
+		// 遷移する際押すマウスの対応した番号
+		[SerializeField]
+		private int _button = 1;
 
-        public override IEnumerator Enter()
-        {
-            yield return null;
-        }
+		public override IEnumerator Enter()
+		{
+			yield return null;
+		}
 
-        public override void EnterComplete()
-        {
-            base.EnterComplete();
-        }
+		public override void EnterComplete()
+		{
+			base.EnterComplete();
+		}
 
-        public override void Exit()
-        {
-            base.Exit();
-        }
+		public override void Exit()
+		{
+			base.Exit();
+		}
 
-        protected void Update()
-        {
-            base.Update();
+		protected void FixedUpdate()
+		{
+			base.FixedUpdate();
 
-            if (Input.GetMouseButtonDown(_button))
-            {
-                // 呼び出しはこれ
-                DisplayManager.Instance.ChangeDisplay(_changeDisplay);
-            }
-        }
+			if (Input.GetMouseButtonDown(_button))
+			{
+				// 呼び出しはこれ
+				DisplayManager.Instance.ChangeDisplay(_changeDisplay);
+			}
+		}
 
-        public override void KeyInput()
-        {
+		public override void KeyInput()
+		{
 
-        }
-    }
+		}
+	}
 }
