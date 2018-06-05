6f74b2ac7d6b65d509fdce35f788b6116681bd92
TSITCL\s162054 <yasu.suika@gmail.com>
2018-06-05 09:33:59 +0900

    ノイズ

---
+++ Assets/Yasu/tekito.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tekito : MonoBehaviour {
+
+Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+		pos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos.x = 9 + Mathf.Sin(Time.time)*1.5f;
+		gameObject.transform.position = pos;
+	}
+}
