18fda59e7fd732a7afc8ffbb94146126ad7450ac
Masashi7 <masashi77823@gmail.com>
2018-06-07 18:04:27 +0900

    タイマークラス (#127)
    * タイマークラス作成
    
    * わすれもの

--- Assets/zNiki/Scripts/Timer.cs
+++ Assets/zNiki/Scripts/Timer.cs
@@ -5,30 +5,32 @@ using UnityEngine.UI;
 
 namespace Play
 {
-    public class PlayTimer : MonoBehaviour
+    public class Timer : MonoBehaviour
     {
-        //分数カウント
+        // 分数カウント
         private int _minuteCount;
-        //経過時間カウント
+        // 秒数カウント
         private float _secondCount;
-        //計測中か
-        [SerializeField]
+        // 経過時間カウント（ミリ秒）
+        private float _msecondCount;
+        // 計測中か
         private bool _isCounting;
-        //GUIスタイル
-        private GUIStyle style;
-        
+        // タイム表示テキスト
+        private Text _timerText;
 
         // Use this for initialization
         void Start()
         {
-            style = new GUIStyle();
-            style.fontSize = 100;
-            style.normal.textColor = new Vector4(1, 1, 1, 1);
             //分数初期化
             _minuteCount = 0;
             //秒数初期化
             _secondCount = 0;
-            _isCounting = true;
+            // 経過時間初期化
+            _msecondCount = 0;
+
+            _isCounting = false;
+
+            _timerText = this.GetComponent<Text>();
         }
 
         // Update is called once per frame
@@ -56,16 +58,18 @@ namespace Play
         {
             _minuteCount = 0;
             _secondCount = 0;
+            _msecondCount = 0;
         }
 
         //タイマー更新
-        void UpdateTimer()
+        private void UpdateTimer()
         {
             //計測中なら
             if (_isCounting)
             {
                 //時間加算
                 _secondCount += Time.deltaTime;
+                _msecondCount += Time.deltaTime;
 
                 if (_secondCount >= 60.0f)
                 {
@@ -73,26 +77,32 @@ namespace Play
                     _secondCount -= 60.0f;
                 }
 
+                DisplayTime();
             }
-         
         }
 
-
-        void OnGUI()
+        // テキストに表示
+        private void DisplayTime()
         {
-            //秒数によって変更
-            if (_secondCount < 10)
-            {
-                GUI.Label(new Rect(370, 50, 100, 50), "Time:" + _minuteCount.ToString("00") + ":0" + _secondCount.ToString("F2"), style);
-
-            }
-            else
-            {
-                GUI.Label(new Rect(370, 50, 100, 50), "Time:" + _minuteCount.ToString("00") + ":" + _secondCount.ToString("F2"), style);
-
-            }
+            _timerText.text = "Time: " + _minuteCount.ToString("00") + ":" + _secondCount.ToString("00.00");
         }
 
-     
+        // タイム取得（分）
+        public int GetMinute()
+        {
+            return _minuteCount;
+        }
+
+        // タイム取得（秒）
+        public float GetSecond()
+        {
+            return _secondCount;
+        }
+        
+        // タイム取得（ミリ秒）
+        public float GetMillisecond()
+        {
+            return _msecondCount;
+        }
     }
 }
---
+++ Assets/zNiki/Scripts/TimerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerTest : MonoBehaviour {
+
+    [SerializeField]
+    private Play.Timer timer;
+
+    [SerializeField]
+    private Text minuteText;
+    [SerializeField]
+    private Text secondText;
+    [SerializeField]
+    private Text millisecondText;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer.StartTimer();
+        minuteText.text = "Min: " + timer.GetMinute().ToString();
+        secondText.text = "Sec: " + timer.GetSecond().ToString();
+        millisecondText.text = "Msec: " + timer.GetMillisecond().ToString();
+    }
+}
