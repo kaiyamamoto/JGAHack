0b137ff2e7442f6d07afd019830ab3c6ad5ee0b3
やまげん <dqx_2527@yahoo.co.jp>
2018-06-13 15:50:58 +0900

    セレクト画面で背景のステージを左右に動かすのではなくカメラをずらすように変更

--- Assets/Scripts/Main/BackStage.cs
+++ Assets/Scripts/Main/BackStage.cs
@@ -6,37 +6,32 @@ using Extensions;
 
 namespace Main
 {
-	public class BackStage : MonoBehaviourEx
-	{
-		private ResourceRequest _stageAsset;
+    public class BackStage : MonoBehaviourEx
+    {
+        private ResourceRequest _stageAsset;
 
-		public void ChangeStage(int num)
-		{
-			StartCoroutine(StageLoad(num));
-		}
+        public void ChangeStage(int num)
+        {
+            StartCoroutine(StageLoad(num));
+        }
 
-		private IEnumerator StageLoad(int num)
-		{
-			// アセットのロード
-			var stageName = "Stage/Stage_" + num;
-			_stageAsset = Resources.LoadAsync(stageName);
+        private IEnumerator StageLoad(int num)
+        {
+            // アセットのロード
+            var stageName = "Stage/Stage_" + num;
+            _stageAsset = Resources.LoadAsync(stageName);
 
-			// ロード待ち
-			yield return new WaitWhile(() => !_stageAsset.isDone);
+            // ロード待ち
+            yield return new WaitWhile(() => !_stageAsset.isDone);
 
-			foreach (var obj in this.transform.GetAllChild())
-			{
-				Destroy(obj);
-			}
+            foreach (var obj in this.transform.GetAllChild())
+            {
+                Destroy(obj);
+            }
 
-			var stageObj = _stageAsset.asset as GameObject;
-			var stage = Instantiate(stageObj);
-			this.transform.SetChild(stage);
-
-			var pos = stage.transform.localPosition;
-			pos.x = -7.0f;
-			stage.transform.localPosition = pos;
-			stage.transform.DOLocalMoveX(pos.x + 10.0f, 5.0f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
-		}
-	}
+            var stageObj = _stageAsset.asset as GameObject;
+            var stage = Instantiate(stageObj);
+            this.transform.SetChild(stage);
+        }
+    }
 }
