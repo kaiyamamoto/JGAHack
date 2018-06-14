a074fb3fb34b0104e57267881697953726337930
slovenlyboy <slovenlyboy@gmail.com>
2018-06-14 20:49:06 +0900

    アイコンの調整

--- Assets/Scripts/Game/ElementObject/ElementContainer.cs
+++ Assets/Scripts/Game/ElementObject/ElementContainer.cs
@@ -49,7 +49,22 @@ namespace Play.Element
             //Debug.Log(_list[2].Type);
 
 
-            
+            for (int i = 0; i < _list.Count; i++)
+            {
+                if (_list[i].Type == ElementType.Action)
+                {
+                    _dataPanel.GetComponent<PlayerDataPanel>().SetIcon(0, _list[i].GetType().Name, GetComponent<DiectionTest>().GetDir());
+                }
+                else if (_list[i].Type == ElementType.Move)
+                {
+                    _dataPanel.GetComponent<PlayerDataPanel>().SetIcon(1, _list[i].GetType().Name, GetComponent<DiectionTest>().GetDir());
+                }
+                else if (_list[i].Type == ElementType.Direction)
+                {
+                    _dataPanel.GetComponent<PlayerDataPanel>().SetIcon(2, _list[i].GetType().Name, GetComponent<DiectionTest>().GetDir());
+                }
+            }
+
             return true;
         }
 
