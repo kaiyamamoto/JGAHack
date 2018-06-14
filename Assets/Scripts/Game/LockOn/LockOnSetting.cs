b06e8c9b012fbaeded28f09de623fe5f1e4ca104
やまげん <dqx_2527@yahoo.co.jp>
2018-06-14 20:10:20 +0900

    Revert "要素オブジェターゲット時に要素を表示していたテキストを破棄"
    This reverts commit a278a423c9d1ebe958448f93d395e5a4af0ebbcd.

--- Assets/Scripts/Game/LockOn/LockOnSetting.cs
+++ Assets/Scripts/Game/LockOn/LockOnSetting.cs
@@ -10,5 +10,9 @@ namespace Play.LockOn
         // ターゲットのゲームオブジェクト
         [SerializeField]
         public GameObject _target = null;
+
+        // TODO:選択したオブジェクトの要素テキスト
+        [SerializeField]
+        public Text _elementText;
     }
 }
--- Assets/Scripts/Game/LockOn/TargetObject.cs
+++ Assets/Scripts/Game/LockOn/TargetObject.cs
@@ -25,12 +25,52 @@ namespace Play.LockOn
             { return _elementObj; }
         }
 
+        private LockOnSetting _setting = null;
+        public LockOnSetting Setting
+        {
+            get { return _setting ? _setting : _setting = Resources.Load("Settings\\LockOnSetting") as LockOnSetting; }
+        }
+
         /// <summary>
         /// 初期化
         /// </summary>
         void InitSetting()
         {
             _elementObj = GetComponent<Element.ElementObject>();
+
+            // TODO: 仮で選択したオブジェクトにテキストを付与
+            // ======================================================
+            // 子に要素追加
+            var setting = Setting;
+            var text = GameObject.Instantiate(setting._elementText);
+            transform.SetChild(text.gameObject);
+            // ターゲットマーカー作成
+            var obj = Instantiate(setting._target);
+            text.transform.SetChild(obj);
+            text.transform.localPosition = Vector3.zero;
+            text.gameObject.AddComponent<Canvas>();
+            var scaler = text.gameObject.AddComponent<CanvasScaler>();
+            scaler.dynamicPixelsPerUnit = 20;
+            text.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+            text.transform.localScale = new Vector3(0.3f, 0.3f, 1.0f);
+            text.fontSize = 1;
+            text.alignment = TextAnchor.MiddleLeft;
+
+            // テキスト変更
+
+            text.text = string.Empty;
+            foreach (var element in ElementObj.ElementList)
+            {
+                if (element)
+                {
+                    text.text += element.Type.ToString() + "\n";
+                }
+            }
+            if (text.text == string.Empty)
+            {
+                text.text = "NoneElement";
+            }
+            // ======================================================
         }
 
         /// <summary>
@@ -57,6 +97,14 @@ namespace Play.LockOn
         public void Release()
         {
             // 選択解除
+            var childs = transform.GetAllChild();
+            foreach (var c in childs)
+            {
+                if (c.name == "ElementText(Clone)")
+                {
+                    Destroy(c.gameObject);
+                }
+            }
             Destroy(this);
 
             _selector.TargetRelease();
