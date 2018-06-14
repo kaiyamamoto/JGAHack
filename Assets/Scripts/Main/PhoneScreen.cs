c9fa195432d7e0567882a0c399f154ef0dbcb565
やまげん <dqx_2527@yahoo.co.jp>
2018-06-14 20:28:20 +0900

    クリアから戻て来た時カーソルを前のステージの場所に移動

--- Assets/Scripts/Main/PhoneScreen.cs
+++ Assets/Scripts/Main/PhoneScreen.cs
@@ -6,108 +6,108 @@ using DG.Tweening;
 
 namespace Main
 {
-	public class PhoneScreen : ScreenBase
-	{
-		// 作成するパネルの数
-		public int STAGE_NUM = 7;
+    public class PhoneScreen : ScreenBase
+    {
+        // 作成するパネルの数
+        public int STAGE_NUM = 7;
 
-		private Tween _moveTween = null;
+        private Tween _moveTween = null;
 
-		public void SetUp()
-		{
-			base.SetUp();
+        public void SetUp()
+        {
+            base.SetUp();
 
-			PanelSlide(0);
-		}
+            PanelSlide(TakeOverData.Instance.StageNum - 1);
+        }
 
-		private Image MovePanel(Vector3 pos, int index)
-		{
-			var obj = _panelList[index];
+        private Image MovePanel(Vector3 pos, int index)
+        {
+            var obj = _panelList[index];
 
-			obj.rectTransform.localScale = Vector3.one;
-			_moveTween = obj.rectTransform.DOLocalMove(pos, 0.15f)
-								.OnComplete(() => _moveTween = null);
+            obj.rectTransform.localScale = Vector3.one;
+            _moveTween = obj.rectTransform.DOLocalMove(pos, 0.15f)
+                                .OnComplete(() => _moveTween = null);
 
-			return obj;
-		}
+            return obj;
+        }
 
-		/// <summary>
-		/// パネルのセットアップ
-		/// </summary>
-		private void PanelSetup()
-		{
-			var create = IsCreated();
+        /// <summary>
+        /// パネルのセットアップ
+        /// </summary>
+        private void PanelSetup()
+        {
+            var create = IsCreated();
 
-			for (int i = 0; i < STAGE_NUM; i++)
-			{
-				var offSet = _popOffSet;
-				var posX = 0.0f;
+            for (int i = 0; i < STAGE_NUM; i++)
+            {
+                var offSet = _popOffSet;
+                var posX = 0.0f;
 
-				if (i == _selectIndex)
-				{
-					posX += offSet.x;
-				}
+                if (i == _selectIndex)
+                {
+                    posX += offSet.x;
+                }
 
-				var posY = i * (_panelRect.y + offSet.y);
-				var pos = new Vector3(_initPos.x - posX, _initPos.y - posY, _initPos.z);
+                var posY = i * (_panelRect.y + offSet.y);
+                var pos = new Vector3(_initPos.x - posX, _initPos.y - posY, _initPos.z);
 
-				if (create)
-				{
-					var obj = CreatePanel(pos, i);
+                if (create)
+                {
+                    var obj = CreatePanel(pos, i);
 
-					// テキスト変更
-					var text = obj.GetComponentInChildren<Text>();
-					text.text = "STAGE " + (i + 1);
-				}
-				else MovePanel(pos, i);
-			}
-		}
+                    // テキスト変更
+                    var text = obj.GetComponentInChildren<Text>();
+                    text.text = "STAGE " + (i + 1);
+                }
+                else MovePanel(pos, i);
+            }
+        }
 
-		/// <summary>
-		/// パネルを次に移動
-		/// </summary>
-		public int PanelNext()
-		{
-			return PanelSlide(1);
-		}
+        /// <summary>
+        /// パネルを次に移動
+        /// </summary>
+        public int PanelNext()
+        {
+            return PanelSlide(1);
+        }
 
-		/// <summary>
-		/// パネルを前に移動
-		/// </summary>
-		public int PanelBefore()
-		{
-			return PanelSlide(-1);
-		}
+        /// <summary>
+        /// パネルを前に移動
+        /// </summary>
+        public int PanelBefore()
+        {
+            return PanelSlide(-1);
+        }
 
-		/// <summary>
-		/// パネルのスライド移動
-		/// </summary>
-		/// <param name="c"></param>
-		private int PanelSlide(int c)
-		{
-			if ((_moveTween != null) ||
-				(!IndexIsContained(_selectIndex + c)))
-			{
-				return -1;
-			}
+        /// <summary>
+        /// パネルのスライド移動
+        /// </summary>
+        /// <param name="c"></param>
+        private int PanelSlide(int c)
+        {
+            if ((_moveTween != null) ||
+                (!IndexIsContained(_selectIndex + c)))
+            {
+                return -1;
+            }
 
-			var margin = _panelRect.y + _popOffSet.y;
-			_initPos.y += (margin * c);
-			_selectIndex += c;
-			PanelSetup();
+            var margin = _panelRect.y + _popOffSet.y;
+            _initPos.y += (margin * c);
+            _selectIndex += c;
+            PanelSetup();
 
-			return _selectIndex;
-		}
+            return _selectIndex;
+        }
 
-		private bool IndexIsContained(int index)
-		{
-			var count = STAGE_NUM;
-			if ((index < 0) || (count <= index))
-			{
-				return false;
-			}
-			return true;
-		}
+        private bool IndexIsContained(int index)
+        {
+            var count = STAGE_NUM;
+            if ((index < 0) || (count <= index))
+            {
+                return false;
+            }
+            return true;
+        }
 
-	}
+    }
 }
