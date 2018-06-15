214de6820f9c07366a228d945131d825d3255cd7
slovenlyboy <slovenlyboy@gmail.com>
2018-06-15 19:23:17 +0900

    弾の更新

---
+++ Assets/Scripts/Game/Animation/BulletAniCon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAniCon : MonoBehaviour {
+
+
+    SimpleAnimation _anim;
+    // Use this for initialization
+ 
+    private void OnEnable()
+    {
+        _anim = GetComponent<SimpleAnimation>();
+    }
+
+    public void ChangeAnim(Direction dir)
+    {
+        
+
+        switch (dir)
+        {
+            case Direction.Front:              
+                _anim.CrossFade("Front", 0);
+                break;
+
+            case Direction.Left:          
+                _anim.CrossFade("Left", 0);
+                break;
+
+            case Direction.Right:            
+                _anim.CrossFade("Right", 0);
+                break;
+
+            case Direction.Back:             
+                _anim.CrossFade("Back", 0);
+                break;
+        }
+    }
+
+
+}
