5977c7319f9351fae66b3f293bfb2b8f2cc8e1ae
やまげん <dqx_2527@yahoo.co.jp>
2018-06-18 10:14:44 +0900

    displaytimeの修正

--- Assets/zNiki/Scripts/Timer.cs
+++ Assets/zNiki/Scripts/Timer.cs
@@ -5,63 +5,63 @@ using UnityEngine.UI;
 
 namespace Play
 {
-	public class Timer : MonoBehaviour
-	{
-		// 経過時間カウント（ミリ秒）
-		private float _msecondCount;
-		// 計測中か
-		private bool _isCounting = false;
+    public class Timer : MonoBehaviour
+    {
+        // 経過時間カウント（ミリ秒）
+        private float _msecondCount;
+        // 計測中か
+        private bool _isCounting = false;
 
-		//時間計測開始
-		public void StartTimer()
-		{
-			if (_isCounting) return;
+        //時間計測開始
+        public void StartTimer()
+        {
+            if (_isCounting) return;
 
-			_isCounting = true;
-			ResetTimer();
+            _isCounting = true;
+            ResetTimer();
 
-			StartCoroutine(TimerCorutine());
-		}
-		private IEnumerator TimerCorutine()
-		{
-			yield return new WaitWhile(() =>
-			{
-				if (!_isCounting) return _isCounting;
-				_msecondCount += Time.deltaTime;
-				return _isCounting;
-			});
-		}
+            StartCoroutine(TimerCorutine());
+        }
+        private IEnumerator TimerCorutine()
+        {
+            yield return new WaitWhile(() =>
+            {
+                if (!_isCounting) return _isCounting;
+                _msecondCount += Time.deltaTime;
+                return _isCounting;
+            });
+        }
 
-		//時間計測停止
-		public void StopTimer()
-		{
-			_isCounting = false;
-		}
+        //時間計測停止
+        public void StopTimer()
+        {
+            _isCounting = false;
+        }
 
-		//時間計測リセット
-		public void ResetTimer()
-		{
-			_msecondCount = 0;
-		}
+        //時間計測リセット
+        public void ResetTimer()
+        {
+            _msecondCount = 0;
+        }
 
-		// テキストに表示
-		static public string DisplayTime(float mScecond)
-		{
-			var time = mScecond;
-			int minute = 0;
-			if (time >= 60f)
-			{
-				minute++;
-				time = time - 60;
-			}
-			return minute.ToString("00") + ":" + time.ToString("00.00");
-		}
+        // テキストに表示
+        static public string DisplayTime(float mScecond)
+        {
+            var time = mScecond;
+            int minute = 0;
+            while (time >= 60f)
+            {
+                minute++;
+                time = time - 60;
+            }
+            return minute.ToString("00") + ":" + time.ToString("00.00");
+        }
 
 
-		// タイム取得（ミリ秒）
-		public float GetMillisecond()
-		{
-			return _msecondCount;
-		}
-	}
+        // タイム取得（ミリ秒）
+        public float GetMillisecond()
+        {
+            return _msecondCount;
+        }
+    }
 }
